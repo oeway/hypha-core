53a8fe4a3abb76c1e0f41af84330a5fc32b392fe
Wei Ouyang <oeway007@gmail.com>
2024-06-23 10:40:01 +0200

    Upgrade imjoy-rpc

--- public/hypha-webpython.js
+++ public/hypha-webpython.js
@@ -1,8 +1,25 @@
 importScripts("https://cdn.jsdelivr.net/pyodide/v0.26.1/full/pyodide.js");
 
 const startupScript = `
+import sys
+import types
 from imjoy_rpc.hypha import setup_local_client
-server = await setup_local_client(enable_execution=True)
+
+async def execute(server, config):
+    print('executing script:', config["name"])
+    for script in config["scripts"]:
+        if script.get("lang") != "python":
+            raise Exception("Only python scripts are supported")
+        imjoyModule = types.ModuleType('imjoy')
+        imjoyModule.api = server
+        sys.modules['imjoy'] = imjoyModule
+        import imjoy
+        import imjoy_rpc
+        imjoy_rpc.api = server
+        sys.modules['imjoy_rpc'] = imjoy_rpc
+        exec(script["content"], {'imjoy': imjoy, 'imjoy_rpc': imjoy_rpc, 'api': server})
+
+server = await setup_local_client(enable_execution=True, execute=execute)
 `
 console.log("Loading Pyodide...");
 loadPyodide().then(async (pyodide) => {
@@ -10,7 +27,7 @@ loadPyodide().then(async (pyodide) => {
     console.log("Pyodide is ready to use.");
     await pyodide.loadPackage("micropip");
     const micropip = pyodide.pyimport("micropip");
-    await micropip.install('imjoy-rpc==0.5.53-post5');
+    await micropip.install('imjoy-rpc==0.5.54-post2');
     const isWindow = typeof window !== "undefined";
     if (isWindow) {
         window.parent.postMessage({ type: "hyphaClientReady" }, "*");
--- public/hypha-webworker.js
+++ public/hypha-webworker.js
@@ -1,4 +1,4 @@
-importScripts("https://cdn.jsdelivr.net/npm/imjoy-rpc@0.5.53/dist/hypha-rpc-websocket.min.js");
+importScripts("https://cdn.jsdelivr.net/npm/imjoy-rpc@0.5.54/dist/hypha-rpc-websocket.min.js");
 hyphaWebsocketClient.setupLocalClient({enable_execution: true}).then((api)=>{
     console.log("Hypha WebWorker initialized.", api)
 }).catch(console.error);
