30a67010c0fc438849f73e82d1c916da52235cb1
Wei Ouyang <oeway007@gmail.com>
2025-06-28 14:46:45 -0700

    auto convert camel case

--- test/integration/debug-console-test.js
+++ test/integration/debug-console-test.js
@@ -1,4 +1,4 @@
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
 test.describe('Debug Console', () => {
     test('should check console errors on page load', async ({ page }) => {
